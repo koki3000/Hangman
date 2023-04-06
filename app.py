69f39cabee2143ec21fb0d696e469c5b5dca7264
Konrad P <kokiziom@gmail.com>
2023-04-06 16:57:39 +0200

    Changed variables naming

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import re
 import getpass
 
-hangmanElementsList = [
+hangman_elements = [
     '',
     """
      |
@@ -96,70 +96,70 @@ class Hangman:
     
     def __init__(self, phrase):
         self.list = []
-        self.wrongGuessCnt = 0
-        self.isInPhrase = False
-        self.phraseGuess = ''
-        self.fullPhrase = phrase
+        self.wrong_guess_cnt = 0
+        self.is_in_phrase = False
+        self.phrase_guess = ''
+        self.full_phrase = phrase
         self.phrase = phrase.split()
-        self.setUp()
+        self.set_up()
     
-    def incWrongCnt(self):
-        self.wrongGuessCnt += 1
+    def inc_wrong_cnt(self):
+        self.wrong_guess_cnt += 1
 
-    def setUp(self):
+    def set_up(self):
         for word in self.phrase:
             for char in word:
-                self.phraseGuess += '_ '
-            self.phraseGuess += '  '
-        print(self.phraseGuess)
+                self.phrase_guess += '_ '
+            self.phrase_guess += '  '
+        print(self.phrase_guess)
 
-    def phraseUpdate(self, guess):
-        self.isInPhrase = False
+    def phrase_update(self, guess):
+        self.is_in_phrase = False
         if len(guess) > 1:
-            if guess == self.fullPhrase:
-                    self.phraseGuess = guess
-                    self.isInPhrase = True
+            if guess == self.full_phrase:
+                    self.phrase_guess = guess
+                    self.is_in_phrase = True
         elif len(guess) == 1:
-            self.phraseGuess = ''
+            self.phrase_guess = ''
             for word in self.phrase:
                 if guess in word:
-                    self.isInPhrase = True
+                    self.is_in_phrase = True
                 for char in word:
                     if char in self.list:
-                        self.phraseGuess += char + ' '
+                        self.phrase_guess += char + ' '
                     else:
-                        self.phraseGuess += '_ '
-                self.phraseGuess += '  '
+                        self.phrase_guess += '_ '
+                self.phrase_guess += '  '
         
         
-        if not self.isInPhrase:
-            self.incWrongCnt()
-        print(hangmanElementsList[self.wrongGuessCnt])
-        print(self.phraseGuess)
+        if not self.is_in_phrase:
+            self.inc_wrong_cnt()
+        print(hangman_elements[self.wrong_guess_cnt]) #todo
+        print(self.phrase_guess)
         print(self.list)
         print(pause)        
     
-    def checkGame(self):
-        if not '_' in self.phraseGuess:            
+    def check_game(self):
+        if not '_' in self.phrase_guess:            
             print('You won')
             return True
-        elif self.wrongGuessCnt == len(hangmanElementsList)-1:
+        elif self.wrong_guess_cnt == len(hangman_elements)-1:
             print('You lost')
             return True
 
-    def addGuess(self):
+    def add_guess(self):
         guess = input('Guess the letter or phrase: ')
         while True:
             if not re.match(r'^[a-zA-Z]+$', guess):
                 guess = input('Please use letter: ')
             elif len(guess) > 1:
-                self.phraseUpdate(guess.lower())
+                self.phrase_update(guess.lower())
                 break
             elif guess in self.list:                
                 guess = input('Letter already used, use another: ')                    
             else:
                 self.list.append(guess.lower())  
-                self.phraseUpdate(guess.lower())
+                self.phrase_update(guess.lower())
                 break
 
 
@@ -169,6 +169,6 @@ if __name__ == '__main__':
         phrase = getpass.getpass('Input phrase to guess (only letters) ')
     game = Hangman(phrase.lower())
     while True:
-        game.addGuess()
-        if game.checkGame():
+        game.add_guess()
+        if game.check_game():
             break
